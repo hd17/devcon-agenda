5f01dbcfb7395935d13c126fba44cc94617da7b8
Andri Bremm <andri.bremm@senacor.com>
2017-10-11 15:55:16 +0200

    Added more talks.

--- src/main/java/com/senacor/hd17/devcon/services/TalksService.java
+++ src/main/java/com/senacor/hd17/devcon/services/TalksService.java
@@ -23,10 +23,28 @@ public class TalksService {
         talk1.setEndTime(ZonedDateTime.parse("2017-10-11T16:00:00+01:00[Europe/Paris]"));
         talks.add(talk1);
 
-        Talk talk2 = new Talk(2, "AWS", new Speaker("Herbert"));
-        talk2.setStartTime(ZonedDateTime.parse("2017-10-12T15:05:30+01:00[Europe/Paris]"));
-        talk2.setEndTime(ZonedDateTime.parse("2017-10-12T16:00:00+01:00[Europe/Paris]"));
+        Talk talk2 = new Talk(1, "Test 2", new Speaker("Hans"));
+        talk2.setStartTime(ZonedDateTime.parse("2017-10-11T16:00:00+01:00[Europe/Paris]"));
+        talk2.setEndTime(ZonedDateTime.parse("2017-10-11T16:45:00+01:00[Europe/Paris]"));
         talks.add(talk2);
+
+        Talk talk3 = new Talk(1, "Test 3", new Speaker("Lisa"));
+        talk3.setStartTime(ZonedDateTime.parse("2017-10-11T17:00:00+01:00[Europe/Paris]"));
+        talk3.setEndTime(ZonedDateTime.parse("2017-10-11T17:45:00+01:00[Europe/Paris]"));
+        talks.add(talk3);
+
+
+
+        Talk talk9 = new Talk(1, "Test 9", new Speaker("Maria"));
+        talk9.setStartTime(ZonedDateTime.parse("2017-10-11T21:00:00+01:00[Europe/Paris]"));
+        talk9.setEndTime(ZonedDateTime.parse("2017-10-11T21:45:00+01:00[Europe/Paris]"));
+        talks.add(talk9);
+
+
+        Talk talk20 = new Talk(2, "AWS", new Speaker("Herbert"));
+        talk20.setStartTime(ZonedDateTime.parse("2017-10-12T15:05:30+01:00[Europe/Paris]"));
+        talk20.setEndTime(ZonedDateTime.parse("2017-10-12T16:00:00+01:00[Europe/Paris]"));
+        talks.add(talk20);
         return talks;
     }
 
